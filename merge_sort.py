bee59ad4849fd038317a27a9349d5233012c3854
emilicos <notalvaroaustin@gmail.com>
2023-11-03 16:13:46 +0700

    feat: making the code more similar to pseudocode

--- merge_sort.py
+++ merge_sort.py
@@ -12,17 +12,17 @@ def merge(A, p, q, r):
     L = [0] * (a + 1)
     R = [0] * (b + 1)
     
-    for i in range(a): # iterasi mulai dari 0 karena indeks array dimulai dari 0
-        L[i] = A[p + i] # Karena mulai dari 0, oleh karena itu, kita juga perlu menyesuaikan ini juga
+    for i in range(1, a + 1):
+        L[i - 1] = A[p + i - 1] # Index L dikurang 1, karena list L mulai dari 0
     
-    for j in range(b):
-        R[j] = A[q + j + 1]
+    for j in range(1, b + 1):
+        R[j - 1] = A[q + j] # Index R dikurang 1, karena list L mulai dari 0
     
     L[a] = float("inf")
     R[b] = float("inf")
     
-    i = 0
-    j = 0
+    i = 0 # i mulai dari 0 karena indeks array dimulai dari 0
+    j = 0 # j mulai dari 0 karena indeks array dimulai dari 0
 
     for k in range(p, r + 1):
         if L[i] <= R[j]:
@@ -32,7 +32,7 @@ def merge(A, p, q, r):
             A[k] = R[j]
             j = j + 1
                         
-x =  [2, 7]
+x =  [-3, -6, -1, -2, -4, -5]
 
 merge_sort(x, 0, len(x) - 1)
 
